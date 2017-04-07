a8542e2a8f7bee5075fb2f449ae3c0c4a1a8405b
Chris <cxg8590@rit.edu>
2017-04-07 09:10:48 -0400

    forgot to run npm test last time

--- server/physics.js
+++ server/physics.js
@@ -1,28 +1,26 @@
 // our socket code for physics to send updates back
 const sockets = require('./sockets.js');
 
-let charList = {}; // list of characters
-let rockets = []; // array of attack to handle
+const rockets = []; // array of attack to handle
 const grav = 10;
 
 const gravity = () => {
-    if(rockets.length > 0){
-    for(var i = 0; i < rockets.length; i++){
-        console.log("bad idea1 Y: " + rockets[i].y);
-        rockets[i].y -= rockets[i].vel;
-        rockets[i].velY -= grav;
-        console.log("bad idea2 y: " + rockets[i].ht);
-        if(rockets[i].ht >= rockets[i].y){
-            rockets[i].vel *= -1;
-        }
-        rockets[i].x += rockets[i].ang * .1;
-        if(rockets[i].x >= 500 || rockets[i].x <= 0){
-            rockets[i].ang *= -1;
-        }
-        
+  if (rockets.length > 0) {
+    for (let i = 0; i < rockets.length; i++) {
+      console.log(`bad idea1 Y: ${rockets[i].y}`);
+      rockets[i].y -= rockets[i].vel;
+      rockets[i].velY -= grav;
+      console.log(`bad idea2 y: ${rockets[i].ht}`);
+      if (rockets[i].ht >= rockets[i].y) {
+        rockets[i].vel *= -1;
+      }
+      rockets[i].x += rockets[i].ang * 0.1;
+      if (rockets[i].x >= 500 || rockets[i].x <= 0) {
+        rockets[i].ang *= -1;
+      }
     }
     sockets.handleFlight(rockets);
-    }
+  }
 };
 
 // add a new attack to calculate physics on
@@ -35,7 +33,7 @@ setInterval(() => {
   gravity();
 }, 20);
 module.exports.addRocket = addRocket;
-/*module.exports.setCharacterList = setCharacterList;
+/* module.exports.setCharacterList = setCharacterList;
 module.exports.setCharacter = setCharacter;
 module.exports.addAttack = addAttack;
 */
--- server/sockets.js
+++ server/sockets.js
@@ -2,7 +2,7 @@
 const xxh = require('xxhashjs');
 const physics = require('./physics.js');
 // Character custom class
-/*const Character = require('./classes/Character.js');
+/* const Character = require('./classes/Character.js');
 // our physics calculation file
 const physics = require('./physics.js');
 
@@ -10,12 +10,12 @@ const physics = require('./physics.js');
 const characters = {};
 */
 let rockets = [];
-let colors = [];
+const colors = [];
 // our socketio instance
 let io;
 
 // function to notify everyone when a user has been hit
-/*const handleAttack = (userHash) => {
+/* const handleAttack = (userHash) => {
   io.sockets.in('room1').emit('attackHit', userHash);
 };
 */
@@ -26,40 +26,40 @@ const setupSockets = (ioServer) => {
 
   // on socket connections
   io.on('connection', (sock) => {
-      console.log("connected");
+    console.log('connected');
     const socket = sock;
 
     socket.join('room1'); // join user to our socket room
-      
-      socket.on('launch', (data) => {
-          console.log("launching: "+data);
-      //rockets[rockets.length -1] = data;
-          const hash = xxh.h32(`${socket.id}${new Date().getTime()}`, 0xCAFEBABE).toString(16);
-          console.log("launching: " + hash);
-          var tempRocket = {
-              x: data.x,
-              y: data.y,
-              ht: data.ht,
-              vel: data.velY,
-              ang: data.ang,
-              out: "000000",
-              in: "FFFFFF",
-              id: hash
-          };
-          console.log("temp y: " + tempRocket.y);
-          console.log("temp ht: " + tempRocket.ht);
-          io.sockets.in('room1').emit('getID', hash);
-        physics.addRocket(tempRocket);
-      });
-      
-      socket.on("colorPack", (data) => {
-          colors.push(data);
-      })
+
+    socket.on('launch', (data) => {
+      console.log(`launching: ${data}`);
+      // rockets[rockets.length -1] = data;
+      const hash = xxh.h32(`${socket.id}${new Date().getTime()}`, 0xCAFEBABE).toString(16);
+      console.log(`launching: ${hash}`);
+      const tempRocket = {
+        x: data.x,
+        y: data.y,
+        ht: data.ht,
+        vel: data.velY,
+        ang: data.ang,
+        out: '000000',
+        in: 'FFFFFF',
+        id: hash,
+      };
+      console.log(`temp y: ${tempRocket.y}`);
+      console.log(`temp ht: ${tempRocket.ht}`);
+      io.sockets.in('room1').emit('getID', hash);
+      physics.addRocket(tempRocket);
+    });
+
+    socket.on('colorPack', (data) => {
+      colors.push(data);
+    });
 
     // when the user disconnects
     socket.on('disconnect', () => {
       // let everyone know this user left
-      /*io.sockets.in('room1').emit('left', characters[socket.hash]);
+      /* io.sockets.in('room1').emit('left', characters[socket.hash]);
       // remove this user from our object
       delete characters[socket.hash];
       // update the character list in our physics calculations
@@ -72,16 +72,16 @@ const setupSockets = (ioServer) => {
 };
 
 const handleFlight = (e) => {
-    rockets = e;
-    for(var i = 0;i < rockets.length; i++){
-        for(var j = 0;j < colors.length; j++){
-            if(rockets[i].id == colors[j].id){
-                rockets[i].out = colors[j].out;
-                rockets[i].in = colors[j].in;
-            }
-        }
+  rockets = e;
+  for (let i = 0; i < rockets.length; i++) {
+    for (let j = 0; j < colors.length; j++) {
+      if (rockets[i].id === colors[j].id) {
+        rockets[i].out = colors[j].out;
+        rockets[i].in = colors[j].in;
+      }
     }
-    io.sockets.in('room1').emit('soaring', rockets);
+  }
+  io.sockets.in('room1').emit('soaring', rockets);
 };
 
 module.exports.setupSockets = setupSockets;
